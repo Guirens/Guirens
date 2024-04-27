7a22697431ca5f703139cf1e202c4865a9aa4fa8
Guilherme Henrique Gonçalves <147348431+Guirens@users.noreply.github.com>
2024-04-27 12:54:06 -0300

    Add files via upload

---
+++ atividade11/src/main/java/br/com/projeto/constant/Constant.java
@@ -0,0 +1,10 @@
+package br.com.projeto.constant;
+
+public class Constant {
+
+    public static final String API_URL = "projeto/api/v1/";
+
+    public static final String API_CLIENT = API_URL + "client";
+
+    public static final String TEST = "test";
+}
